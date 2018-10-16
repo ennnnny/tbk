12b6ea958ca441986d190828d0018de8df118324
ennnnny <kuye1130@gmail.com>
2018-10-16 20:09:47 +0800

    兼容PHP7之前版本

--- src/Kernel/Support/Helpers.php
+++ src/Kernel/Support/Helpers.php
@@ -98,7 +98,7 @@ function current_url()
 {
     $protocol = 'http://';
 
-    if ((!empty($_SERVER['HTTPS']) && 'off' !== $_SERVER['HTTPS']) || ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? 'http') === 'https') {
+    if ((!empty($_SERVER['HTTPS']) && 'off' !== $_SERVER['HTTPS']) || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) ? $_SERVER['HTTP_X_FORWARDED_PROTO'] : 'http') === 'https') {
         $protocol = 'https://';
     }
 
